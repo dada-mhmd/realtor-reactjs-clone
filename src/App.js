35836249ce8c92bc644450a8d41a556c7abbf870
dada-mhmd <moudi.ld@hotmail.com>
2022-11-27 16:48:58 +0200

    created pages and set routes with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ForgotPassowrd from './pages/ForgotPassowrd'
+import Home from './pages/Home'
+import Offers from './pages/Offers'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 function App() {
   return (
     <>
-      <h1 className='text-2xl text-red-400'>hello world</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassowrd />} />
+          <Route path='/offers' element={<Offers />} />
+        </Routes>
+      </Router>
     </>
   )
 }
