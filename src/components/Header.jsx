7c588fadf50192f519123f2239992a2acde03e84
dada-mhmd <moudi.ld@hotmail.com>
2022-11-30 13:15:28 +0200

    added spinner component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,30 @@
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { useEffect, useState } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 
 const Header = () => {
+  const [pageState, setPageState] = useState('Sign in')
+
   const location = useLocation()
   const navigate = useNavigate()
+  const auth = getAuth()
+
+  useEffect(() => {
+    onAuthStateChanged(auth, user => {
+      if (user) {
+        setPageState('Profile')
+      } else {
+        setPageState('Sign in')
+      }
+    })
+  }, [auth])
 
   const pathMatchRoute = route => {
-    if (route === location.pathname) {
-      return true
-    }
+    if (route === location.pathname) return true
   }
 
   return (
-    <div className='bg-white border-b shadow-sm sticky top-0 z-50'>
+    <div className='bg-white border-b shadow-sm sticky top-0 z-40'>
       <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
         <div>
           <img
@@ -42,11 +55,12 @@ const Header = () => {
 
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMatchRoute('/signin') && 'text-black border-b-red-500'
+                pathMatchRoute('/signin') ||
+                (pathMatchRoute('/profile') && 'text-black border-b-red-500')
               }`}
-              onClick={() => navigate('/signin')}
+              onClick={() => navigate('/profile')}
             >
-              Sign in
+              {pageState}
             </li>
           </ul>
         </div>
